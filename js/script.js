6b00c6fc99fd27610523ce46afd55f48a6e4d7a3
NICOLA-200 <fatepepe66@gmail.com>
2023-11-13 19:55:37 +0200

    onscrolling working

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id')
